1f7a9562bb1f9f38271abd7466cd24476c076be1
hebercelis <ojitos_h@hotmail.com>
2018-12-03 04:51:26 -0600

    avance_heberRamirez
    se agrego un enemigo nuevo con script para el  movimiento y deteccion del jugador y seguirlo, tambien un estado de patrullaje del mismo

---
+++ Shooter/Assets/Scripts/detectarjug.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detectarjug : MonoBehaviour
+{
+    public Transform player;
+    UnityEngine.AI.NavMeshAgent enemy;
+    private bool range = false;
+    public Transform[] waypoints;
+    Vector3 targetP;
+    public float limitR;
+    public float speed = 3;
+    private float health;
+    private float damage;
+
+
+    // Use this for initialization
+
+    void Start()
+    {
+        targetP = waypoints[0].position;
+        enemy = GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    private void move()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetP, Time.deltaTime * speed);
+
+        
+    }
+    void changet()
+    {
+        int randomI = Random.Range(0, waypoints.Length);
+        targetP = waypoints[randomI].position;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            range = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            range = false;
+        }
+    }
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, targetP) < limitR)
+        {
+            changet();
+        }
+        move();
+        
+        if (!range)
+        {
+            enemy.destination = player.position;
+        }
+        if (range)
+        {
+            enemy.destination = this.transform.position;
+        }
+    }
+}
